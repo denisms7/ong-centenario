60a585d441a3edd9c9fdf3d908c101cce2ceabb4
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2023-01-26 16:51:54 -0300

    added tabelas especie e raça

--- ongcentenario/pet/admin.py
+++ ongcentenario/pet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Especie, Raca
 
 # Register your models here.
+admin.site.register(Especie)
+admin.site.register(Raca)
--- ongcentenario/pet/models.py
+++ ongcentenario/pet/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Especie(models.Model):
+    nome = models.CharField(max_length=150, verbose_name='Nome', unique=True, null=False)
+
+    def __str__(self):
+        return f"{self.nome}"
+
+
+class Raca(models.Model):
+    especie = models.ForeignKey(Especie, on_delete=models.PROTECT, verbose_name='Especie')
+    raca = models.CharField(max_length=150, verbose_name='Raça')
+
+    def __str__(self):
+        return f"{self.especie.nome} - {self.raca}"
+

bde3c09b4d75ef90498f8d6f17319f13731d763f
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-01-26 18:30:12 -0300

    added tabelas sexo e idade

--- ongcentenario/pet/admin.py
+++ ongcentenario/pet/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Especie, Raca
+from .models import Especie, Raca, Sexo, Idade
 
 # Register your models here.
 admin.site.register(Especie)
 admin.site.register(Raca)
+admin.site.register(Idade)
+admin.site.register(Sexo)
--- ongcentenario/pet/models.py
+++ ongcentenario/pet/models.py
@@ -16,3 +16,16 @@ class Raca(models.Model):
     def __str__(self):
         return f"{self.especie.nome} - {self.raca}"
 
+
+class Idade(models.Model):
+    idade = models.CharField(max_length=10, verbose_name='Idade', unique=True, null=False)
+
+    def __str__(self):
+        return f"{self.idade}"
+
+
+class Sexo(models.Model):
+    sexo = models.CharField(max_length=10, verbose_name='Sexo', unique=True, null=False)
+
+    def __str__(self):
+        return f"{self.sexo}"

0ac0aca634d2c960f4d1a3313e15ea1ab8c35cf7
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2023-01-27 17:02:48 -0300

    att url adocao

--- ongcentenario/pet/urls.py
+++ ongcentenario/pet/urls.py
@@ -7,6 +7,6 @@ from .views import PaginaAdotar, PaginaCadastroAdotar, PaginaCadastroAdotarEdit
 urlpatterns = [
     path('adotar', PaginaAdotar.as_view(), name='adotar'),
 
-    path('adocao/added', PaginaCadastroAdotar.as_view(), name='added-pet'),
+    path('adocao/added/', PaginaCadastroAdotar.as_view(), name='added-pet'),
     path('adocao/edit/<int:pk>', PaginaCadastroAdotarEdit.as_view(), name='editar-pet'),
 ]

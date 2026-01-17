8ed8ee2a3e1e1eb5faad1371bc15eb85422b42be
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2026-01-17 20:23:46 -0300

    added

--- pet/urls.py
+++ pet/urls.py
@@ -2,15 +2,12 @@ from django.urls import path
 from .views import PaginaAdotar, PaginaCadastroAdotar, PaginaCadastroAdotarEdit, PaginaCadastroAdotarDelete, PaginaAdotarLista, PerfilPet
 
 
-
-
 urlpatterns = [
     path('adocao', PaginaAdotar.as_view(), name='adotar'),
     path('adocao/added/', PaginaCadastroAdotar.as_view(), name='added-pet'),
     path('adocao/edit/<int:pk>', PaginaCadastroAdotarEdit.as_view(), name='edit-pet'),
     path('adocao/delete/<int:pk>', PaginaCadastroAdotarDelete.as_view(), name='delete-pet'),
     path('adocao/lista/', PaginaAdotarLista.as_view(), name='lista-pet'),
-
 
     path('adocao/perfil/<int:pk>', PerfilPet.as_view(), name='perfil-pet'),
 ]
